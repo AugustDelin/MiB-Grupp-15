c6817b267592371a15cb152861ce5105c6d4c4fa
ErikE109 <erik.eriksson109@gmail.com>
2022-05-12 11:40:13 +0200

    test

--- src/mib/grupp/pkg15/AgentAlienInfo.java
+++ src/mib/grupp/pkg15/AgentAlienInfo.java
@@ -30,42 +30,37 @@ public class AgentAlienInfo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        spAlienPlats = new javax.swing.JScrollPane();
-        cbAlienPlats = new javax.swing.JComboBox<>();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
         txtAreaListaAliens = new javax.swing.JTextArea();
+        cbAlienPlats = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        cbAlienPlats.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        spAlienPlats.setViewportView(cbAlienPlats);
-
         txtAreaListaAliens.setColumns(20);
         txtAreaListaAliens.setRows(5);
-        jScrollPane1.setViewportView(txtAreaListaAliens);
+        jScrollPane2.setViewportView(txtAreaListaAliens);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(106, 106, 106)
-                        .addComponent(spAlienPlats, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(74, 74, 74)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(92, Short.MAX_VALUE))
+                .addGap(110, 110, 110)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(58, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(cbAlienPlats, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(157, 157, 157))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(93, 93, 93)
-                .addComponent(spAlienPlats, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(73, Short.MAX_VALUE))
+                .addGap(67, 67, 67)
+                .addComponent(cbAlienPlats, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(94, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,8 +73,7 @@ public class AgentAlienInfo extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cbAlienPlats;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane spAlienPlats;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea txtAreaListaAliens;
     // End of variables declaration//GEN-END:variables
 }
--- src/mib/grupp/pkg15/Navigera.java
+++ src/mib/grupp/pkg15/Navigera.java
@@ -14,21 +14,25 @@ public class Navigera {
         
     }
     
+    //metod som skapar en instans av klassen StartSkärm och visar denna
     public static void openStartSkärm()
     {
         new StartSkärm().setVisible(true);
     }
     
+    //metod som skapar en instans av klassen AlienStartSkärm och visar denna
     public static void openAlienStartSkärm(String användarnamn)
     {
         new AlienStartSkärm(användarnamn).setVisible(true);
     }
     
+    //metod som skapar en instans av klassen AgentStartSkärm och visar denna
     public static void openAgentStartSkärm(String användarnamn)
     {
         new AgentStartSkärm(användarnamn).setVisible(true);
     }
     
+    //metod som öppnar en ny sida av AgentMinSIda
     public static void openAgentMinSida(String användarnamn)
     {
         new AgentMinSida(användarnamn).setVisible(true);
