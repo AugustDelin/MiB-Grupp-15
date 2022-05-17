7fa26a3c3bcced4abbf25b7ec4136acbc6bd1480
LindaRos90 <105203083+LindaRos90@users.noreply.github.com>
2022-05-17 14:06:20 +0200

    ttt

--- src/mib/grupp/pkg15/AgentRegistreraAlien.java
+++ src/mib/grupp/pkg15/AgentRegistreraAlien.java
@@ -145,7 +145,7 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("Linda");
+        jLabel1.setText("Linda1");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
