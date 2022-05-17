7fb5042fc4ce4ae1a2af691254b878fd51686015
ErikE109 <erik.eriksson109@gmail.com>
2022-05-17 11:23:25 +0200

    add

--- src/mib/grupp/pkg15/AgentRegistreraAlien.java
+++ src/mib/grupp/pkg15/AgentRegistreraAlien.java
@@ -39,14 +39,31 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
         lblInloggadSom = new javax.swing.JLabel();
         lblRegistreraAlien = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        jSplitPane1 = new javax.swing.JSplitPane();
+        lblRubrikRegd = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        lbldagensDatum = new javax.swing.JLabel();
+        jSplitPane2 = new javax.swing.JSplitPane();
+        lbRubrikID = new javax.swing.JLabel();
+        lblvisaID = new javax.swing.JLabel();
+        jSplitPane3 = new javax.swing.JSplitPane();
+        lblRubrikNamn = new javax.swing.JLabel();
+        txtNamn = new javax.swing.JTextField();
+        jSplitPane4 = new javax.swing.JSplitPane();
+        lblRubrikRas = new javax.swing.JLabel();
+        cbRaser = new javax.swing.JComboBox<>();
+        jSplitPane5 = new javax.swing.JSplitPane();
+        lblRubrikLösen = new javax.swing.JLabel();
+        txtLösenord = new javax.swing.JPasswordField();
+        jSplitPane6 = new javax.swing.JSplitPane();
+        lblRubrikTelefon = new javax.swing.JLabel();
+        txtTelefon = new javax.swing.JTextField();
+        jSplitPane7 = new javax.swing.JSplitPane();
+        lblRubrikPlats = new javax.swing.JLabel();
+        cbPlatser = new javax.swing.JComboBox<>();
+        jSplitPane8 = new javax.swing.JSplitPane();
+        lblRubrikAgent = new javax.swing.JLabel();
+        cbAgenter = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -75,63 +92,93 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
 
         lblRegistreraAlien.setText("Registrera alien");
 
-        jLabel1.setText("ID-nummer");
+        lblRubrikRegd.setText("Registreringsdatum");
+        jSplitPane1.setLeftComponent(lblRubrikRegd);
 
-        jLabel2.setText("Namn");
+        jTextField1.setText("jTextField1");
+        jSplitPane1.setRightComponent(jTextField1);
 
-        jLabel3.setText("Ras");
+        lbldagensDatum.setText("Dagens datum");
+        jSplitPane1.setRightComponent(lbldagensDatum);
 
-        jLabel4.setText("Telefonnummer");
+        lbRubrikID.setText("ID-nummer");
+        jSplitPane2.setLeftComponent(lbRubrikID);
 
-        jLabel5.setText("Plats");
+        lblvisaID.setText("ett ID");
+        jSplitPane2.setRightComponent(lblvisaID);
 
-        jLabel6.setText("Ansvarig Agent");
+        lblRubrikNamn.setText("Namn");
+        jSplitPane3.setLeftComponent(lblRubrikNamn);
 
-        jLabel7.setText("Lösenord");
+        txtNamn.setText("jTextField2");
+        jSplitPane3.setRightComponent(txtNamn);
 
-        jLabel8.setText("Registreringsdatum");
+        lblRubrikRas.setText("Ras");
+        jSplitPane4.setLeftComponent(lblRubrikRas);
+
+        cbRaser.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jSplitPane4.setRightComponent(cbRaser);
+
+        lblRubrikLösen.setText("Lösenord");
+        jSplitPane5.setLeftComponent(lblRubrikLösen);
+        jSplitPane5.setRightComponent(txtLösenord);
+
+        lblRubrikTelefon.setText("Telefonnummer");
+        jSplitPane6.setLeftComponent(lblRubrikTelefon);
+
+        txtTelefon.setText("jTextField3");
+        jSplitPane6.setRightComponent(txtTelefon);
+
+        lblRubrikPlats.setText("Plats");
+        jSplitPane7.setLeftComponent(lblRubrikPlats);
+
+        cbPlatser.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jSplitPane7.setRightComponent(cbPlatser);
+
+        lblRubrikAgent.setText("Ansvarig Agent");
+        jSplitPane8.setLeftComponent(lblRubrikAgent);
+
+        cbAgenter.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jSplitPane8.setRightComponent(cbAgenter);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, 155, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jSplitPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSplitPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSplitPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSplitPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSplitPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSplitPane7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSplitPane8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jLabel1)
-                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                            .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                        .addContainerGap()
+                        .addComponent(jSplitPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(242, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(jSplitPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(24, 24, 24)
-                .addComponent(jLabel8)
+                .addComponent(jSplitPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
+                .addComponent(jSplitPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23)
+                .addComponent(jSplitPane4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25)
+                .addComponent(jSplitPane5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addComponent(jSplitPane6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jSplitPane7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(jLabel3)
-                .addGap(31, 31, 31)
-                .addComponent(jLabel7)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addGap(27, 27, 27)
-                .addComponent(jLabel5)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel6)
-                .addContainerGap(82, Short.MAX_VALUE))
+                .addComponent(jSplitPane8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(59, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -139,11 +186,14 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addComponent(lblInloggadSom)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btnBacka)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(btnBacka)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(182, 182, 182)
@@ -151,12 +201,11 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(lblRegistreraAlien, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(121, 121, 121)
-                                .addComponent(btnLoggaut))
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblInloggadSom)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                                .addComponent(btnLoggaut))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 441, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -168,7 +217,7 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
                     .addComponent(lblRegistreraAlien))
                 .addGap(18, 18, 18)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 52, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAvsluta)
                     .addComponent(btnBacka))
@@ -197,16 +246,33 @@ public class AgentRegistreraAlien extends javax.swing.JFrame {
     private javax.swing.JButton btnAvsluta;
     private javax.swing.JButton btnBacka;
     private javax.swing.JButton btnLoggaut;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JComboBox<String> cbAgenter;
+    private javax.swing.JComboBox<String> cbPlatser;
+    private javax.swing.JComboBox<String> cbRaser;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JSplitPane jSplitPane2;
+    private javax.swing.JSplitPane jSplitPane3;
+    private javax.swing.JSplitPane jSplitPane4;
+    private javax.swing.JSplitPane jSplitPane5;
+    private javax.swing.JSplitPane jSplitPane6;
+    private javax.swing.JSplitPane jSplitPane7;
+    private javax.swing.JSplitPane jSplitPane8;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel lbRubrikID;
     private javax.swing.JLabel lblInloggadSom;
     private javax.swing.JLabel lblRegistreraAlien;
+    private javax.swing.JLabel lblRubrikAgent;
+    private javax.swing.JLabel lblRubrikLösen;
+    private javax.swing.JLabel lblRubrikNamn;
+    private javax.swing.JLabel lblRubrikPlats;
+    private javax.swing.JLabel lblRubrikRas;
+    private javax.swing.JLabel lblRubrikRegd;
+    private javax.swing.JLabel lblRubrikTelefon;
+    private javax.swing.JLabel lbldagensDatum;
+    private javax.swing.JLabel lblvisaID;
+    private javax.swing.JPasswordField txtLösenord;
+    private javax.swing.JTextField txtNamn;
+    private javax.swing.JTextField txtTelefon;
     // End of variables declaration//GEN-END:variables
 }
