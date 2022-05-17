f9d88cccee3e3f8202b3bda53349d230800a5d59
AugustDelin <august.del@live.se>
2022-05-17 13:46:12 +0200

    nyregistrera alien fix

--- src/mib/grupp/pkg15/AgentMetoder.java
+++ src/mib/grupp/pkg15/AgentMetoder.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
@@ -318,6 +319,22 @@ public class AgentMetoder {
         } catch (InfException ex) {
             Logger.getLogger(AgentMetoder.class.getName()).log(Level.SEVERE, null, ex);
         }
+        }
+    }
+    
+    public static void nyRegistreraAlien(JLabel id, JLabel datum, JTextField namnFält, JComboBox rasLåda, JPasswordField lösenFält, JTextField telNrFält, JComboBox platsLåda, JComboBox agentLåda) {
+        try {
+            int ettID = id.getText();
+            String ettDatum = datum.getText();
+            String ettNamn = namnFält.getText();
+            String enRas = rasLåda.getSelectedItem().toString();
+            String ettLösen = lösenFält.getText();
+            String ettTelNr = telNrFält.getText();
+            int enPlats = platsLåda.getText();
+            int enAgent = agentLåda.getText();
+            idb.insert("insert into alien values(" + ettID + ",'" + ettDatum + "','" + ettLösen + "','" + ettNamn + "','" + enRas + "','" + ettTelNr + "'" + enPlats + "," + enAgent);
+        } catch (InfException ex) {
+            Logger.getLogger(AgentMetoder.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
