e2da86aa65fc7d9f84d20b5425bebc8353bd5ff0
ErikE109 <erik.eriksson109@gmail.com>
2022-05-20 09:57:15 +0200

    Update AdminMetoder.java

--- src/mib/grupp/pkg15/AdminMetoder.java
+++ src/mib/grupp/pkg15/AdminMetoder.java
@@ -290,8 +290,8 @@ public class AdminMetoder {
         try {
             String valdUtrustning = Validera.hamtaCbSträng(enLåda);
             int utrustningsID = GetMetoder.hämtaUtrustningsIDFrånNamn(valdUtrustning);
-            idb.delete("delete from utrustning where benamning ='" + valdUtrustning + "'");
             idb.delete("delete from innehar_utrustning where Utrustnings_ID =" + utrustningsID);
+            idb.delete("delete from utrustning where benamning ='" + valdUtrustning + "'");
             JOptionPane.showMessageDialog(null, "Du har tagit bort " + valdUtrustning + " ur systemet");
             enLåda.removeItem(valdUtrustning);
         } catch (InfException ex) {
