607a2d5e7c5ad6551fe4c3cd9fcca8d420dc6b7a
ErikE109 <erik.eriksson109@gmail.com>
2022-05-25 11:22:16 +0200

    topptrelista

--- src/mib/grupp/pkg15/AgentTopplista.java
+++ src/mib/grupp/pkg15/AgentTopplista.java
@@ -9,17 +9,18 @@ package mib.grupp.pkg15;
  * @author augustdelin
  */
 // Fälten för AgentAlienInfo.
-public class AgentOCinfo extends javax.swing.JFrame {
+public class AgentTopplista extends javax.swing.JFrame {
 
     private String användarnamn;
 
     /**
      * Konstruktorn för AgentOCInfo.
      */
-    public AgentOCinfo(String användarnamn) {
+    public AgentTopplista(String användarnamn) {
         initComponents();
         this.användarnamn = användarnamn;
         FyllText.inloggadSom(lblInloggadSom, användarnamn);
+        ComboBoxar.fyllCBområden(cbOmråden);
 
     }
 
@@ -33,20 +34,19 @@ public class AgentOCinfo extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane2 = new javax.swing.JScrollPane();
-        txtAreaListaChefer = new javax.swing.JTextArea();
+        txtAreaTopplista = new javax.swing.JTextArea();
         btnBacka = new javax.swing.JButton();
         btnAvsluta = new javax.swing.JButton();
         btnLoggaUt = new javax.swing.JButton();
         lblInloggadSom = new javax.swing.JLabel();
-        btnVisaOmrådeschefer = new javax.swing.JButton();
-        btnVisaKontorschefer = new javax.swing.JButton();
+        cbOmråden = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        txtAreaListaChefer.setEditable(false);
-        txtAreaListaChefer.setColumns(20);
-        txtAreaListaChefer.setRows(5);
-        jScrollPane2.setViewportView(txtAreaListaChefer);
+        txtAreaTopplista.setEditable(false);
+        txtAreaTopplista.setColumns(20);
+        txtAreaTopplista.setRows(5);
+        jScrollPane2.setViewportView(txtAreaTopplista);
 
         btnBacka.setText("Föregående Sida");
         btnBacka.addActionListener(new java.awt.event.ActionListener() {
@@ -71,17 +71,9 @@ public class AgentOCinfo extends javax.swing.JFrame {
 
         lblInloggadSom.setText("Inloggad Som");
 
-        btnVisaOmrådeschefer.setText("Visa Områdeschefer");
-        btnVisaOmrådeschefer.addActionListener(new java.awt.event.ActionListener() {
+        cbOmråden.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnVisaOmrådescheferActionPerformed(evt);
-            }
-        });
-
-        btnVisaKontorschefer.setText("Visa Kontorschefer");
-        btnVisaKontorschefer.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnVisaKontorscheferActionPerformed(evt);
+                cbOmrådenActionPerformed(evt);
             }
         });
 
@@ -91,24 +83,20 @@ public class AgentOCinfo extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(lblInloggadSom)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(btnVisaOmrådeschefer)
-                        .addContainerGap())
-                    .addComponent(btnLoggaUt, javax.swing.GroupLayout.Alignment.TRAILING)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 295, Short.MAX_VALUE)
+                .addComponent(btnLoggaUt))
             .addGroup(layout.createSequentialGroup()
                 .addComponent(btnBacka)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(btnAvsluta))
             .addGroup(layout.createSequentialGroup()
-                .addGap(110, 110, 110)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(104, Short.MAX_VALUE))
+                .addGap(77, 77, 77)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 293, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnVisaKontorschefer)
-                .addContainerGap())
+                .addComponent(cbOmråden, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(187, 187, 187))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -116,13 +104,11 @@ public class AgentOCinfo extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblInloggadSom)
                     .addComponent(btnLoggaUt))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnVisaKontorschefer)
-                .addGap(11, 11, 11)
-                .addComponent(btnVisaOmrådeschefer)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                .addComponent(cbOmråden, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnBacka)
                     .addComponent(btnAvsluta))
@@ -147,13 +133,9 @@ public class AgentOCinfo extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_btnLoggaUtActionPerformed
 
-    private void btnVisaOmrådescheferActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVisaOmrådescheferActionPerformed
-       MetoderAgentAdmin.listaAllaOmrådesChefer(txtAreaListaChefer);
-    }//GEN-LAST:event_btnVisaOmrådescheferActionPerformed
-
-    private void btnVisaKontorscheferActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVisaKontorscheferActionPerformed
-        MetoderAgentAdmin.listaAllaKontorsChefer(txtAreaListaChefer);
-    }//GEN-LAST:event_btnVisaKontorscheferActionPerformed
+    private void cbOmrådenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbOmrådenActionPerformed
+       MetoderAgentAdmin.toppTreAnsvarigaAgenterValtOmråde(cbOmråden, txtAreaTopplista);
+    }//GEN-LAST:event_cbOmrådenActionPerformed
 
     /**
      * @param args the command line arguments
@@ -163,10 +145,9 @@ public class AgentOCinfo extends javax.swing.JFrame {
     private javax.swing.JButton btnAvsluta;
     private javax.swing.JButton btnBacka;
     private javax.swing.JButton btnLoggaUt;
-    private javax.swing.JButton btnVisaKontorschefer;
-    private javax.swing.JButton btnVisaOmrådeschefer;
+    private javax.swing.JComboBox<String> cbOmråden;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lblInloggadSom;
-    private javax.swing.JTextArea txtAreaListaChefer;
+    private javax.swing.JTextArea txtAreaTopplista;
     // End of variables declaration//GEN-END:variables
 }
